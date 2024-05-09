cb201577ed2c9b5897cf53e239cbaaf9308d7d48
deepakrajm <deepakraj.m@shield.com>
2024-05-09 16:09:20 +0530

    version update to 1.5.38

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let version = "1-5-35"
-let shieldcredit_checksum = "093a248ad0f650d9645b0b9f107957e954a236bdd600aff2ee48e4022472ce4b"
+let version = "1-5-38"
+let shieldcredit_checksum = "15357d60b7b28ec4ad869345a14169531615661aef63176b710a7c186f0f8e3b"
 
 let package = Package(
     name: "shieldcredit-spm",
@@ -13,14 +13,24 @@ let package = Package(
     products: [
         .library(
             name: "ShieldCredit",
-            targets: ["ShieldCredit"]
+            targets: ["ShieldCreditTarget"]
         )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/shield-ai-technology/shieldfraud-spm.git", branch: "main")
     ],
     targets: [
         .binaryTarget(
             name: "ShieldCredit",
             url: "https://s3.amazonaws.com/cashshield-sdk/shieldcredit-ios-swift-\(version).zip",
             checksum: shieldcredit_checksum
-        )
+        ),
+        .target(
+            name: "ShieldCreditTarget",
+            dependencies: [
+              .target(name: "ShieldCredit"),
+              .product(name: "ShieldFraud", package: "shieldfraud-spm")
+            ]
+          )
     ]
 )
